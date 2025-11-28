3dace1e7ba28f11ea328179c952bddda3832ec2a
“pantrypoints” <pantrypoints@gmail.com>
2025-11-28 16:02:47 +0800

    update

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt'; 
+import * as bcrypt from 'bcryptjs'; 
 import { encodeBase32LowerCase } from '@oslojs/encoding';
 import { fail, redirect } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
